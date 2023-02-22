d5fa6ce11a41a1865dd0abf7808b8d8d09235b0f
CarloMonroy <carlomonroy1997@gmail.com>
2023-02-22 16:25:01 -0600

    add cors

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ const morganMiddleware = require("./middlewares/morgan.middleware");
 const logger = require("./utils/logger");
 // import orm
 const { Sequelize } = require("sequelize");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const sequelize = new Sequelize(
   "mysql://root:FlisrTniB3gRNVyZmGXC@containers-us-west-109.railway.app:6382/railway"
 );
